108975af6f5c4c2dab49dc71cd70c6e536604ecf
Patrick van de Glind <patrick_de_glind@hotmail.com>
2022-01-29 11:30:57 +0100

    Updated outdated-deps to Antq

--- scripts/outdated-deps.sh
+++ scripts/outdated-deps.sh
@@ -1,6 +1,4 @@
-clj -X:deps find-versions :lib org.clojure/clojure | tail -1
-clj -X:deps find-versions :lib org.clojure/data.json | tail -1
-clj -X:deps find-versions :lib org.clojure/tools.cli | tail -1
-clj -X:deps find-versions :lib org.clojars.crowbrammer/csv-exporter | tail -1
-clj -X:deps find-versions :lib io.github.clojure/tools.build | tail -1
-clj -X:deps find-versions :lib io.github.cognitect-labs/test-runner | tail -1
+# using Antq: https://github.com/liquidz/antq
+# install with:
+# clojure -Ttools install com.github.liquidz/antq '{:git/tag "1.4.0"}' :as antq
+clojure -Tantq outdated
