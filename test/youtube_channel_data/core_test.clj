99c8f60ae2d7dd80f5ddd1146d972ff1d8dac4d3
Aaron Bell <iam@aarondeobell.com>
2022-02-11 03:31:56 -0500

    improve test messages

--- test/youtube_channel_data/core_test.clj
+++ test/youtube_channel_data/core_test.clj
@@ -3,14 +3,20 @@
             [youtube-channel-data.core :as ytcd-core]))
 
 (deftest video-id->channel-id-test
-  (is (= (ytcd-core/video-id->channel-id "1DQ0j_9Pq-g") "UCkDtCKtPKlsg-gJO_m5D0mQ"))
-  (is (= (ytcd-core/video-id->channel-id "ntkpK9gwEU4") "UCLA_DiR1FfKNvjuUpBHmylQ"))
-  (is (nil? (ytcd-core/video-id->channel-id "invalid-string"))))
+  (is (= (ytcd-core/video-id->channel-id "1DQ0j_9Pq-g") "UCkDtCKtPKlsg-gJO_m5D0mQ")
+    "Given a Telepurte video id, this should return his channel ID.")
+  (is (= (ytcd-core/video-id->channel-id "ntkpK9gwEU4") "UCLA_DiR1FfKNvjuUpBHmylQ")
+    "Given a NASA video id, this should return NASA's channel ID.")
+  (is (nil? (ytcd-core/video-id->channel-id "invalid-string"))
+    "Invalid strings should return nil (for now)."))
 
 (deftest channel-id->playlist-id+title-test
   (is (= (ytcd-core/channel-id->playlist-id+title "UCkDtCKtPKlsg-gJO_m5D0mQ")
-        ["UUkDtCKtPKlsg-gJO_m5D0mQ" "Telepurte"]))
+        ["UUkDtCKtPKlsg-gJO_m5D0mQ" "Telepurte"])
+    "Given Telepurte's channel id, this should return a vector of TP's main playlist ID and channel name.")
   (is (= (ytcd-core/channel-id->playlist-id+title "UCLA_DiR1FfKNvjuUpBHmylQ")
-        ["UULA_DiR1FfKNvjuUpBHmylQ" "NASA"]))
+        ["UULA_DiR1FfKNvjuUpBHmylQ" "NASA"])
+    "Given NASA's channel id, this should return a vector of NASA's main playlist ID and channel name.")
   (is (= (ytcd-core/channel-id->playlist-id+title "invalid-string")
-        [nil nil])))
+        [nil nil])
+    "Invalid strings should return a vector of nils (for now :( )"))
