3f613ddccd28ec7185a1026310c2e73d2491152a
Aaron Bell <iam@aarondeobell.com>
2022-02-11 03:27:30 -0500

    test channel-id->playlist-id+title

--- test/youtube_channel_data/core_test.clj
+++ test/youtube_channel_data/core_test.clj
@@ -6,3 +6,11 @@
   (is (= (ytcd-core/video-id->channel-id "1DQ0j_9Pq-g") "UCkDtCKtPKlsg-gJO_m5D0mQ"))
   (is (= (ytcd-core/video-id->channel-id "ntkpK9gwEU4") "UCLA_DiR1FfKNvjuUpBHmylQ"))
   (is (nil? (ytcd-core/video-id->channel-id "invalid-string"))))
+
+(deftest channel-id->playlist-id+title-test
+  (is (= (ytcd-core/channel-id->playlist-id+title "UCkDtCKtPKlsg-gJO_m5D0mQ")
+        ["UUkDtCKtPKlsg-gJO_m5D0mQ" "Telepurte"]))
+  (is (= (ytcd-core/channel-id->playlist-id+title "UCLA_DiR1FfKNvjuUpBHmylQ")
+        ["UULA_DiR1FfKNvjuUpBHmylQ" "NASA"]))
+  (is (= (ytcd-core/channel-id->playlist-id+title "invalid-string")
+        [nil nil])))
